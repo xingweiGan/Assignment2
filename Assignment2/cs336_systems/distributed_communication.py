cdd8e84c21fb478e8e7dea7596f75c1cfca54fb2
XW <xigan@ucsd.edu>
2025-08-04 02:34:40 +0000

    Stuck issue

--- Assignment2/cs336_systems/distributed_communication.py
+++ Assignment2/cs336_systems/distributed_communication.py
@@ -5,27 +5,27 @@ import torch.distributed as dist
 import torch.multiprocessing as mp
 
 def setup(rank, world_size):
-    os.environ["MASTER_ADDR"] = "127.0.0.1"
-    os.environ["MASTER_PORT"] = "9776"
-    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    os.environ["MASTER_ADDR"] = "localhost" #Internal IP address.
+    os.environ["MASTER_PORT"] = "20005"
+    dist.init_process_group("nccl", rank=rank, world_size=world_size) #Q: when using gloo, super fast but run on CPU, when using nccl, sloer but on GPU.
     torch.cuda.set_device(rank)
 
 def benchmark_allreduce(rank, world_size):
     setup(rank, world_size)
-
     device = torch.device(f"cuda:{rank}")
-    tensor = torch.ones(500_000_000, device=device) * rank  # ~2 GB per GPU
-
-    dist.barrier()  # synchronize
+    tensor = torch.ones(500_000_000, device=device) * rank  # ~2 GB per GPU, create tensor on GPU.
     start = time.time()
-    dist.all_reduce(tensor)
-    dist.barrier()
+    dist.all_reduce(tensor, async_op=False)
     end = time.time()
-
+    torch.cuda.synchronize()
     print(f"[Rank {rank}] All-reduce on GPU took {end - start:.3f} seconds")
 
-    dist.destroy_process_group()
+
+    # PyTorch recommended cleanup sequence:
+    torch.cuda.synchronize()        # 1. Wait for GPU operations-not all reduce but maybe sth like cleanup.
+    dist.barrier()                  # 2. Let all CPU wait for each other.  
+    dist.destroy_process_group()    # 3. Clean NCCL for all CPU processes.
 
 if __name__ == "__main__":
     world_size = torch.cuda.device_count()
-    mp.spawn(benchmark_allreduce, args=(world_size,), nprocs=world_size)
+    mp.spawn(benchmark_allreduce, args=(world_size,), nprocs=world_size,join=True)
